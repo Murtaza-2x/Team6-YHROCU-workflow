9dd1ce337746e6ba05a2c9bb0c7afd2828c52b1e
Thomas Kershaw <tom@kershaw.org>
2025-03-13 11:29:09 +0000

    Update SQL table

--- SOFTWARE TEAM 6/htdocs/rocu.sql
+++ SOFTWARE TEAM 6/htdocs/rocu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2025 at 10:44 AM
+-- Generation Time: Mar 13, 2025 at 12:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tasks` (
   `id` int(11) NOT NULL,
-  `subject` varchar(100) NOT NULL,
-  `project` varchar(35) NOT NULL,
-  `assignee` int(11) NOT NULL,
-  `status` set('New','In Progress','Complete','') NOT NULL,
-  `priority` set('Low','Moderate','High','Urgent') NOT NULL
+  `subject` varchar(100) DEFAULT NULL,
+  `project` varchar(35) DEFAULT NULL,
+  `assignee` int(11) DEFAULT NULL,
+  `status` set('New','In Progress','Complete','') DEFAULT NULL,
+  `priority` set('Low','Moderate','High','Urgent') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -41,9 +41,36 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `subject`, `project`, `assignee`, `status`, `priority`) VALUES
-(1, 'First Task', 'Project One', 0, 'New', 'Urgent'),
-(2, 'Second Task', 'Project One', 0, 'New', 'Moderate'),
-(3, 'Third Task', 'Project One', 0, 'New', 'High');
+(1, 'First', 'Project', 0, 'New', 'Low'),
+(2, 'Second', 'Project', 0, 'Complete', 'High'),
+(3, 'Review Changes', 'Developing Helpful Tool', 2, 'In Progress', 'Moderate'),
+(4, 'A', 'B', 1, '', 'High'),
+(5, 'New', 'Project', 3, 'Complete', 'High'),
+(6, 'Newer Task', 'Project Two', 5, 'In Progress', 'Low'),
+(8, 'Add Functionality', 'Project Three', 3, 'In Progress', 'Moderate'),
+(9, 'Example Task', 'Example Project', 12, 'New', 'High');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(35) DEFAULT NULL,
+  `clearance` enum('user','manager','admin','') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `clearance`) VALUES
+(1, 'johnAdmin', 'adminPass', 'admin'),
+(2, 'joeManager', 'managerPass', 'manager'),
+(3, 'jimUser', 'userPass', 'user');
 
 --
 -- Indexes for dumped tables
@@ -56,6 +83,12 @@ ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +96,12 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
