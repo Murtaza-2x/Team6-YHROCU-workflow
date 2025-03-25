3f2820b743e1cf3868b3d6314fdd78e39645a129
Murtaza <murtaza13144@gmail.com>
2025-03-25 12:22:30 +0000

    created a basic emall sending system that doesn't require Gmail API and can run locally. The create-task-page.php was updated to get user emails to send a notification upon task creation

---
+++ SOFTWARE TEAM 6/htdocs/INCLUDES/inc_basicEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+USE PHPMailer\PHPMailer\Exception;
+
+require_once __DIR__.'/../../libs/PHPMailer/src/PHPMailer.php';
+require_once __DIR__.'/../../libs/PHPMailer/src/SMTP.php';
+require_once __DIR__.'/../../libs/PHPMailer/src/Exception.php';
+
+
+function sendTaskEmail ($toEmail) {
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '15ae02232bf29d';
+        $mail->Password = '68f352cc509c23';
+
+        // Email details
+        $mail->setFrom('yhrocunotifications@gmail.com', 'Task Notification');
+        $mail->addAddress($toEmail);
+
+        // Email body
+        $mail->isHTML(true);
+        $mail->Subject = 'A new task has been assigned';
+        $mail->Body = '<p>Hi,</p><p>A new task has been assigned to you</p>';
+
+        $mail->send();
+    
+    } catch (Exception $e) {
+        error_log("Mailtrap email faild: {$mail->ErrorInfo}");
+    }
+}
+?>
