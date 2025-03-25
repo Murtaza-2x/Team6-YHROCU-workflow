ed03f611fa6f4a0edc55c32ff4d34ff3b6c64a6b
fanjum2 <f.anjum2@bradford.ac.uk>
2025-03-25 03:34:06 +0000

    Reworked inc_dashboard.php to correctly display user email and correct clearance on the side. - removed error message that was previously being displayed due to not fetching information correctly.

--- SOFTWARE TEAM 6/htdocs/INCLUDES/inc_dashboard.php
+++ SOFTWARE TEAM 6/htdocs/INCLUDES/inc_dashboard.php
@@ -1,3 +1,4 @@
+
 <head>
   <title><?php echo $title; ?></title>
   <link href="CSS/pill_styles.css" rel="stylesheet">
@@ -6,7 +7,7 @@
   <link href="CSS/tasklist_styles.css" rel="stylesheet">
 </head>
 
-<!-- DASHBOARD SECTION -->
+<!-- DASHBOARD SECTION --> 
 <div class="DASH-CONTENT">
   <div class="DASH-HEADER">
     <p class="DASH-HEADER-1">Dashboard -</p>
@@ -15,8 +16,7 @@
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
-
-    // Check if session variables are set and not empty
+    
     // Check if session variables are set and not empty 
     $email = isset($_SESSION["email"]) ? $_SESSION["email"] : 'Guest';  // Default to 'Guest' if not set
     $clearance = isset($_SESSION["clearance"]) ? $_SESSION["clearance"] : 'Unknown';  // Default to 'Unknown' if not set
