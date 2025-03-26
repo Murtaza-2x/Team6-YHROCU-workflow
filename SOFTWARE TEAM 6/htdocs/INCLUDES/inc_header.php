1f38f4f7addcb6ef0a2d066618ff42c55cc1ec03
fanjum2 <fanjum2@bradford.ac.uk>
2025-03-26 20:41:07 +0000

    renamed logout_button.css to buttons.css and added buttons to inc_header.php home page button and logout button will show when signed in - admin button will show when admin clearance is detected.

--- SOFTWARE TEAM 6/htdocs/INCLUDES/inc_header.php
+++ SOFTWARE TEAM 6/htdocs/INCLUDES/inc_header.php
@@ -11,17 +11,20 @@ $id = $_SESSION['id'];
 <link href="CSS/default_styles.css" rel="stylesheet">
 <link href="CSS/root_colors.css" rel="stylesheet">
 <link href="CSS/header_footer_styles.css" rel="stylesheet">
-<link href="CSS/logout-button.css" rel="stylesheet">
-
+<link href="CSS/buttons.css" rel="stylesheet">
 <body>
 
   <!-- TOP SECTION -->
   <div class="TOP-SECTION">
     <img src="IMAGES/ROCU.png" class="TOP-HERO-IMAGE"></img>
   </div>
-  
+
   <?php if (isset($_SESSION['id'])): ?>
   <div class="logout-button-container">
+    <a href="list-task-page.php" class="home-button">Home</a>
+    <?php if (isset($_SESSION['clearance']) && $_SESSION['clearance'] === 'Admin'): ?>
+      <a href="admin-page.php" class="admin-button">Admin Panel</a>
+    <?php endif; ?>
     <a href="INCLUDES/inc_logout.php" class="logout-button">Logout</a>
   </div>
 <?php endif; ?>
