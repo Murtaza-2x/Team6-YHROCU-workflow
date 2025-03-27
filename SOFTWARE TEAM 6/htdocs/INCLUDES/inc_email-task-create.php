48ccacc0f5f2ced424b30a8c5394a2d38aaa85aa
Murtaza <murtaza13144@gmail.com>
2025-03-27 23:58:12 +0000

    correction to include statement and function name associated with task creation notification email

--- SOFTWARE TEAM 6/htdocs/INCLUDES/inc_email-task-create.php
+++ SOFTWARE TEAM 6/htdocs/INCLUDES/inc_email-task-create.php
@@ -8,7 +8,7 @@ require_once __DIR__.'/../../libs/PHPMailer/src/SMTP.php';
 require_once __DIR__.'/../../libs/PHPMailer/src/Exception.php';
 
 
-function sendTaskEmail ($toEmail) {
+function sendTaskCreateEmail ($toEmail) {
     $mail = new PHPMailer(true);
 
     try {
