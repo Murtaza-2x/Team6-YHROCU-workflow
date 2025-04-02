9b83a55a9bba4289df03d7afaa963ed88a43eaf1
fanjum2 <fanjum2@bradford.ac.uk>
2025-04-02 11:02:18 +0100

    Fixed mail phpmailer directory.

--- SOFTWARE_LOCAL/INCLUDES/inc_basicemail.php
+++ SOFTWARE_LOCAL/INCLUDES/inc_basicemail.php
@@ -3,9 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 USE PHPMailer\PHPMailer\Exception;
 
-require_once __DIR__.'/../../libs/PHPMailer/src/PHPMailer.php';
-require_once __DIR__.'/../../libs/PHPMailer/src/SMTP.php';
-require_once __DIR__.'/../../libs/PHPMailer/src/Exception.php';
+require_once __DIR__.'/../libs/PHPMailer/src/PHPMailer.php';
+require_once __DIR__.'/../libs/PHPMailer/src/SMTP.php';
+require_once __DIR__.'/../libs/PHPMailer/src/Exception.php';
 
 
 function sendTaskEmail ($toEmail) {
